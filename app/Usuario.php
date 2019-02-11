5092bc1c8033065267e290dcd6df58839d4c7805
Cleiton Silva Barbosa <cleitonsbarbosa@gmail.com>
2019-02-10 23:06:16 -0200

    [UPDATE]
    Autenticacao

--- app/Usuario.php
+++ app/Usuario.php
@@ -2,10 +2,58 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model
+class Usuario extends Authenticatable implements JWTSubject
 {
     protected $table = 'tb_usuario';
+
+    protected $primaryKey = 'co_usuario';
+
     public $timestamps = false;
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome', 'email', 'senha', 'sexo', 'co_bairro', 'co_perfil',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'senha',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->co_usuario;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'nome' => $this->nome,
+            'email' => $this->email
+        ];
+    }
 }
